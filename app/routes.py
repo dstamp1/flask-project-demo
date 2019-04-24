91a0a9df93b870d7e62caff860356b9af9b15800
Derek Stampone <derekstampone@gmail.com>
2019-04-24 03:04:30 +0000

    initialize

---
+++ app/routes.py
@@ -0,0 +1,29 @@
+from app import app
+from app.models import model
+from flask import render_template
+from flask import request
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'name': 'Alejandra'}
+    return render_template('index.html', title='Home', user=user)
+@app.route('/secret')
+def secret():
+    return "<h1>You found the secret!</h1>"
+    
+@app.route('/sendBreakfast', methods=['GET', 'POST'])
+def handleBreakfast():
+    if request.method == 'GET':
+        return "You're getting the breakfast page!"
+    else:
+        userdata = dict(request.form)
+        # Store the nickname in a variable for easy reference
+        nickname = userdata['nickname'] # Note that we access the first item in the list provided by the "nickname key"
+        # Store the breakfast in a variable for easy reference
+        breakfast = userdata['breakfast']
+        nickname = model.shout(userdata['nickname'])
+        breakfast = model.shout(userdata['breakfast'])
+        # Use those variables to create a dynamic result for our user
+        return "Hello, " + nickname + "! I hear you had " + breakfast + " for breakfast! Sounds delicious."
