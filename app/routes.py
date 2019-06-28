c5a20d70873171072d71460e3320f79f97fc1e6f
Derek Stampone <dstampone@schools.nyc.gov>
2019-06-27 21:32:23 -0400

    Update project to a pig latin translator

--- app/routes.py
+++ app/routes.py
@@ -7,27 +7,13 @@ from flask import request
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'name': 'Derek'}
-    return render_template('index.html', title='Home', user=user)
-    
-@app.route('/user/<name>')
-def user_page(name):
-    user = {'name': name}
-    return render_template('index.html', title='Home', user=user)
-    
-    
-@app.route('/sendBreakfast', methods=['GET', 'POST'])
-def handleBreakfast():
-    if request.method == 'GET':
-        return "You're getting the breakfast page!"
-    else:
-        userdata = dict(request.form)
-        # Store the nickname in a variable for easy reference
-        nickname = userdata['nickname'] # Note that we access the first item in the list provided by the "nickname key"
-        # Store the breakfast in a variable for easy reference
-        breakfast = userdata['breakfast']
-        
-        nickname = model.shout(userdata['nickname'])
-        breakfast = model.shout(userdata['breakfast'])
-        # Use those variables to create a dynamic result for our user
-        return "Hello, " + nickname + "! I hear you had " + breakfast + " for breakfast! Sounds delicious."
+    return render_template('index.html')
+
+@app.route('/piglatinify', methods=['POST'])
+def piglatinify():
+    userdata = dict(request.form)
+
+    english_sentence = userdata['inputsentence']
+    piglatin_sentence = model.piglatinify(english_sentence)
+
+    return render_template('piglatinify.html', english_sentence=english_sentence, piglatin_sentence=piglatin_sentence)
